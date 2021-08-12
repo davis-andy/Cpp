bec88c65c711c5db0d10a59ef6c578d1d98fdbb5
davis-andy <davis.andy1021@gmail.com>
2021-08-12 10:04:52 -0700

    Update HomeworkAssignment6_2.cpp

--- CIS 014 - C++ Programming/Assignment 6/HomeworkAssignment6_2.cpp
+++ CIS 014 - C++ Programming/Assignment 6/HomeworkAssignment6_2.cpp
@@ -17,7 +17,7 @@ using namespace std;
 
 /**
  * FUNCTION NAME: plusPlusN
- * PURPOSE: 
+ * PURPOSE:
  *      A function that simulates ++N on a vector
  * PARAMETER:
  * 		vector<int> v
@@ -29,28 +29,37 @@ using namespace std;
 
 vector<int> plusPlusN(vector<int> v)
 {
-    int i = v.size()-1;
-    
-    for (i; i >= 0; i--)
+    int temp = v[v.size() - 1];
+
+    if (v[v.size() - 1] == 9)
     {
-        if (i == v.size()-1)
+        v[v.size() - 1] = 0;
+    }
+    else
+    {
+        v[v.size() - 1]++;
+    }
+
+    for (int i = v.size() - 2; i >= 0; i--)
+    {
+        if ((temp == 9) && (v[i + 1] != temp))
         {
+            temp = v[i];
             if (v[i] == 9)
             {
                 v[i] = 0;
             }
-            else v[i]++;
-        }
-        else if (v[i+1] == 0)
-        {
-             if (v[i] == 9)
+            else
             {
-                v[i] = 0;
+                v[i]++;
             }
-            else v[i]++;
+        }
+        else
+        {
+            temp = v[i];
         }
     }
-    
+
 
     vector<int>::iterator it = v.begin();
 
@@ -64,7 +73,7 @@ vector<int> plusPlusN(vector<int> v)
 
 
 int main() {
-    vector<int> v {9, 9};
+    vector<int> v{ 9, 9 };
     vector <int> retVal = plusPlusN(v); // retVal = {1, 0, 0}
     // insert you own assert or other test code here
     vector<int>::iterator i = retVal.begin();
@@ -74,64 +83,6 @@ int main() {
         cout << *i;
     }
     cout << "}";
-    
+
     return 0;
 }
-
-    /*
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 0]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 1]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 1] - TEST FAILED -
-    
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 1]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 2]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 2] - TEST FAILED - 
-    
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 2]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 3]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 3] - TEST FAILED - 
-    
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 3]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 4]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 4] - TEST FAILED -
-    
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 4]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 5]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 5] - TEST FAILED -
-    
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 5]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 6]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 6] - TEST FAILED -
-    
-    INPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 6]
-    YOUR OUTPUT: [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 1, 0, 7]
-    EXPECTED:    [1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 1, 9, 4, 0, 0, 7] - TEST FAILED -
-    
-    INPUT: [1, 0, 0]
-    YOUR OUTPUT: [2, 0, 1]
-    EXPECTED:    [1, 0, 1] - TEST FAILED -
-    
-    INPUT: [1, 0, 1]
-    YOUR OUTPUT: [2, 0, 2]
-    EXPECTED:    [1, 0, 2] - TEST FAILED -
-    
-    INPUT: [1, 0, 0]
-    YOUR OUTPUT: [2, 0, 1]
-    EXPECTED:    [1, 0, 1] - TEST FAILED -
-    
-    INPUT: [1, 0, 1]
-    YOUR OUTPUT: [2, 0, 2]
-    EXPECTED:    [1, 0, 2] - TEST FAILED -
-    
-    INPUT: [1, 0, 2]
-    YOUR OUTPUT: [2, 0, 3]
-    EXPECTED:    [1, 0, 3] - TEST FAILED -
-    
-    INPUT: [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-    YOUR OUTPUT: [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
-    EXPECTED:       [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1] - TEST FAILED -
-    
-    INPUT: [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
-    YOUR OUTPUT: [1, 0, 0, 1, 0, 1, 0, 1, 0, 1, 0, 2]
-    EXPECTED:       [9, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2]
-    */
